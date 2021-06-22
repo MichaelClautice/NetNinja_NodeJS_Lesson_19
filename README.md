0869bde106310cb511b9c77e9b351998b47733ad
Michael Clautice <michaelclauticeusa@gmail.com>
2021-06-22 11:05:27 -0400

    Mike modified 3 files & created 2 new files Tue, Jun 22, 2021

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ NodeJS Tutorial for Beginners #19 - Basic Routing
 
 https://www.youtube.com/watch?v=_zvWeGwVkCY&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp&index=20
 
-How to set up basic routing and respond to different client requests, including a 404 pg to catch any requests that do not correspond to a particular route.
+This lesson demos basic routing w/ 4 client requests & 4 corresponding server responses.
+
+A 404.html pg is included as a response to requests that do not correspond to a particular route.
--- app.js
+++ app.js
@@ -3,31 +3,69 @@
 // Lesson 19 - Basic Routing
 //-----------
 // WHAT THIS APP DOES------
-// This lesson’s application xxxxxxxxxxx 
-// xxxxxxxxxxxxx
+// This lesson’s application demos basic routing... 
+// how an app's server responds to 4 different URL reqs:
+// 1 - home pg, 2 - contact pg, 3 - JSON data, 4 - a pg that doesn't exist
 
 // REQUIRE THE CORE MODULES------
-// srvr requires th http core mod
+// a srvr requires nodejs's http core mod
 const http = require('http');
-// responding w html files requires file system core mod
+// responding w html files requires nodejs's file system core mod
 const fs = require('fs');
 
 // CREATE THE SERVER------
-// create & assign a srvr
-// utilize NodeJS's req & res objs
+// create & assign a srvr to a var for future use
+// utilize HTTP's req & res objs
 const server = http.createServer(function (req, res)
 {
 	// CON.LOG A TEST MSSG-------
 	console.log('\nHey look, the client made this URL request: ' + req.url);
-    // CHECK CLIENT REQ VIA IF-ELSE STATEMENT
-    if (req.url === '/home' || req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/index.html').pipe(res);
-    } 
-
-
-
-
+	// CHECK CLIENT REQ VIA ELSE-IF STATEMENT---------
+	// if client reqs th home pg
+	if (req.url === '/home' || req.url === '/')
+	{
+		res.writeHead(200,
+		{
+			'Content-Type': 'text/html'
+		});
+		fs.createReadStream(__dirname + '/index.html').pipe(res);
+	}
+	// if client reqs th contact pg
+	else if (req.url === '/contact')
+	{
+		res.writeHead(200,
+		{
+			'Content-Type': 'text/html'
+		});
+		fs.createReadStream(__dirname + '/contact.html').pipe(res);
+	}
+	// if client reqs some JSON data
+	else if (req.url === '/api/ninjas')
+	{
+		var ninjas = [
+		{
+			name: 'ryu',
+			age: 29
+		},
+		{
+			name: 'yoshi',
+			age: 32
+		}];
+		res.writeHead(200,
+		{
+			'Content-type': 'application/json'
+		});
+		res.end(JSON.stringify(ninjas));
+	}
+	// if client reqs none of the above
+	else
+	{
+		res.writeHead(404,
+		{
+			'Content-type': 'text/html'
+		});
+		fs.createReadStream(__dirname + '/404.html').pipe(res);
+	}
 });
 
 // LISTEN FOR THE PORT NUMBER------
